fcf850f55e6013c26ac28636ef0ffdbdfe55ca1a
saitovmarat <maratsajtov@gmail.com>
2022-10-13 16:32:36 +0300

    Добавьте файлы проекта.

---
+++ DZ_08.10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DZ_08._10
+{
+    class Program
+    {
+        struct Citizen
+        {
+            public string name;
+            public int pasport;
+            public string problem;
+            public int problem_num;
+            public int agresion_temp;
+            public int smart_temp;
+            public Citizen(string name, int pasport, string problem, int problem_num, int agresion_temp, int smart_temp)
+            {
+                this.name = name;
+                this.pasport = pasport;
+                this.problem = problem;
+                this.problem_num = problem_num;
+                this.agresion_temp = agresion_temp;
+                this.smart_temp = smart_temp;
+            }
+        }
+        static void Task2(int[] first_team, int[] second_team)
+        {
+            int cnt1 = 0;
+            int cnt2 = 0;
+            for (int i = 0; i < first_team.Length; i++)
+            {
+                if ((int)first_team[i] == 5)
+                {
+                    cnt1++;
+                }
+            }
+            for (int j = 0; j < second_team.Length; j++)
+            {
+                if (second_team[j] == 5)
+                {
+                    cnt2++;
+                }
+            }
+            if (cnt1 == cnt2)
+            {
+                Console.WriteLine("Drinks All Round!Free Beers on Bjorg!");
+            }
+            else
+            {
+                Console.WriteLine("Ой, Бьорг - пончик! Ни для кого пива!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] first_team = new int[] {1, 5, 6, 5, 5, 7, 8 };
+            int[] second_team = new int[] {1, 5, 6, 5, 5, 5, 5};
+            Task2(first_team, second_team);
+        }
+    }
+}
---
+++ кр/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace кр
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArr = new int[4, 5];
+            Random ran = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    myArr[i, j] = ran.Next(1, 15);
+                    Console.Write("{0}\t", myArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
