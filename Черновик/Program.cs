6d78f5486d3ec3276f135b59557bef65855cade2
saitovmarat <maratsajtov@gmail.com>
2022-10-19 16:23:10 +0300

    сделал

--- Черновик/Program.cs
+++ Черновик/Program.cs
@@ -5,46 +5,88 @@ using System.Linq;
 
 namespace Черновик
 {
-    class Node 
+    class Student
     {
-        public Node() { }
-        public Node(int value, Node left, Node right) 
+        public string sec_name;
+        public string name;
+        public Student(string a, string b)
         {
-            this.value = value;
-            this.left = left;
-            this.right = right;
+            sec_name = a;
+            name = b;
         }
-        int value;
-        Node left, right;
+        public Student() { }
     }
-
+    class Other_info
+    {
+        public DateTime dob;
+        public string exam;
+        public int points;
+        public Other_info(string d, string e, int p)
+        {
+            dob = DateTime.Parse(d);
+            exam = e;
+            points = p;
+        }
+    }
     class Program
     {
-        public void foo()
-        {
-            Console.WriteLine("foo in main class");
-        }
-
-        public static void boo()
-        {
-            Console.WriteLine("goo in main class | static");
-        }
-
         static void Main(string[] args)
         {
-            //Node root = new Node(1, new Node(7, new Node(5, null, new Node(2, null, null)), new Node(0, null, null)), new Node(4, null, null));
+            Dictionary<Student, Other_info> students = new Dictionary<Student, Other_info>()
+            {
+                {new Student("Саитов", "Марат"), new Other_info("21.05.2004", "Информатика", 236) },
+                {new Student("Маратов", "Саид"), new Other_info("24.02.2005", "Английский", 231) },
+                {new Student("Прохоров", "Прохор"), new Other_info("14.01.2004", "Физика", 225) },
+                {new Student("Нагимов", "Нагим"), new Other_info("22.02.2003", "Физика", 276) },
+                {new Student("Халакаев", "Лакух"), new Other_info("11.04.2004", "Информатика", 216) },
+                {new Student("Преколов", "Цукош"), new Other_info("14.11.2005", "Информатика", 186) },
+                {new Student("Мраков", "Долшан"), new Other_info("12.02.2004", "Информатика", 289) },
+                {new Student("Жамранов", "Жабраил"), new Other_info("30.12.2004", "Физика", 256) },
+                {new Student("Кратов", "Пилан"), new Other_info("11.09.2004", "Информатика", 283) },
+                {new Student("Ладов", "Кирон"), new Other_info("17.03.2003", "Английский", 272) },
+                {new Student("Йодаев", "Валан"), new Other_info("07.07.2004", "Информатика", 259) }
+            };
 
 
-            Program program = new Program();
+            Console.WriteLine("Напиши <<Новый студент>>, если хочешь добавить студентов.");
+            Console.WriteLine("Напиши <<Удалить>>, если хочешь удалить студента.");
+            Console.WriteLine("Напиши <<Сортировать>>, если хочешь отсортировать студентов по баллам по возрастанию.");
+            Console.WriteLine("Напиши <<Стоп>>, если хочешь остановиться.");
+            int n = 1;
+            while (n == 1)
+            {
+                string answer = Console.ReadLine();
+                if (answer == "Новый студент")
+                {
+                    Console.WriteLine("Напиши информацию о нем по порядку: Фамилия, Имя, Дата Рождения, Экзамен, с которым он поступил и его общий балл по всем предметам");
+                    string answ_sec_name = Console.ReadLine();
+                    string answ_name = Console.ReadLine();
+                    string aob = Console.ReadLine();
+                    string exam_of_stud = Console.ReadLine();
+                    int points_stud = int.Parse(Console.ReadLine());
+                    students.Add();//(answ_sec_name, answ_name, aob, exam_of_stud, points_stud);
+                }
+                else if (answer == "Удалить")
+                {
+                    Console.WriteLine("Введи фамилию и Имя человека которого хочешь удалить");
+                    string fam = Console.ReadLine();
+                    string nam = Console.ReadLine();
+                    students.Remove(); //();
 
-            program.foo();
-            Program.boo();
+                }
+                else if (answer == "Сортировать")
+                {
+                    foreach (var numb in students.Values)
+                    {
 
-
-            //root.foo();
-            //root.boo();
-
-            //foo();
+                    }
+                }
+                else if (answer == "Стоп")
+                {
+                    n--;
+                }
+            }
+                
         }
     }
 }
--- Черновик1/Program.cs
+++ Черновик1/Program.cs
@@ -7,65 +7,46 @@ namespace Черновик1
 {
     class Program
     {
-        static void Print(int[,] a)
-        {
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    Console.Write("{0} ", a[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-        static int[,] Multiplication(int[,] a, int[,] b)
-        {
-            if (a.GetLength(1) != b.GetLength(0))
-            {
-                throw new Exception("Матрицы нельзя перемножить");
-            }
-            int[,] r = new int[a.GetLength(0), b.GetLength(1)];
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < b.GetLength(1); j++)
-                {
-                    for (int k = 0; k < b.GetLength(0); k++)
-                    {
-                        r[i, j] += a[i, k] * b[k, j];
-                    }
-                }
-            }
-            return r;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите размерность первой матрицы: ");
-            int[,] first_matrix = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
-            for (int i = 0; i < first_matrix.GetLength(0); i++)
+            Dictionary<int, Student> students = new Dictionary<int, Student>()
             {
-                for (int j = 0; j < first_matrix.GetLength(1); j++)
-                {
-                    Console.Write("first_matrix[{0},{1}] = ", i, j);
-                    first_matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            Console.WriteLine("Введите размерность второй матрицы: ");
-            int[,] second_matrix = new int[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
-            for (int i = 0; i < second_matrix.GetLength(0); i++)
+                { 1, new Student( "Петров", "Петр", "10.10.2000", 100, 150) },
+                { 2, new Student( "Иванов", "Иван", "12.09.2003", 120, 80) },
+                { 3, new Student( "Сидоров", "Сидор", "14.05.2001", 155, 165) }
+
+            };
+
+            foreach (KeyValuePair<int, Student> p in students)
             {
-                for (int j = 0; j < second_matrix.GetLength(1); j++)
-                {
-                    Console.Write("second_matrix[{0},{1}] = ", i, j);
-                    second_matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
+                Console.WriteLine($"Студент Id: {p.Key} " +
+                                    $"\n   Фамилия: {p.Value.Family} " +
+                                    $"\n   Имя: {p.Value.Name} " +
+                                    $"\n   Год рождения: {p.Value.DOB.ToShortDateString()} " +
+                                    $"\n   Входной балл: {p.Value.EntranceMark} " +
+                                    $"\n   Баллы: {p.Value.Mark}");
             }
-            Console.WriteLine("\nПервая матрица:");
-            Print(first_matrix);
-            Console.WriteLine("\nВторая матрица:");
-            Print(second_matrix);
-            Console.WriteLine("\nТретья матрица  = первая матрица * вторая матрица:");
-            int[,] third_matrix = Multiplication(first_matrix, second_matrix);
-            Print(third_matrix);
+
+            Console.Read();
+        }
+
+        class Student
+        {
+            public string Family { get; set; }
+            public string Name { get; set; }
+            public DateTime DOB { get; set; }
+            public int EntranceMark { get; set; }
+            public int Mark { get; set; }
+
+
+            public Student(string f, string n, string d, int e = 0, int m = 0)
+            {
+                Family = f;
+                Name = n;
+                DOB = DateTime.Parse(d);
+                EntranceMark = e;
+                Mark = m;
+            }
         }
     }
 }
